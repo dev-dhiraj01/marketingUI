4a5bc770f3a462e86b83f626447bc7b0fa4cbddb
Dhiraj70C <dchaudhari1256@gmail.com>
2026-01-19 19:39:30 +0530

    added functions related to states

--- src/components/section1/RightCardContent.jsx
+++ src/components/section1/RightCardContent.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { MoveRight } from "lucide-react";
 
 const RightCardContent = (props) => {
+
+  
   return (
     <div className="absolute top-0 left-0 h-full w-full  p-6 flex flex-col justify-between">
       <h2 className="bg-white rounded-full h-9 text-2xl font-bold w-9 flex justify-center items-center">
-       {props.id+1}
+        {props.id + 1}
       </h2>
       <div className="text-white text-shadow-2xs text-sm mt-20">
         <p>
@@ -14,7 +16,12 @@ const RightCardContent = (props) => {
         </p>
       </div>
       <div className=" flex justify-between w-full  ">
-        <button className="bg-blue-600 text-white font-xs px-4 py-2 rounded-full text-sm">
+        <button
+          onClick={() => {
+            console.log("Button clicked!");
+          }}
+          className="bg-blue-600 text-white font-xs px-4 py-2 rounded-full text-sm"
+        >
           {props.tag}
         </button>
         <button className="bg-blue-600 text-white font-medium px-4 py-1 rounded-full text-sm">
--- src/components/section2/Section2.jsx
+++ src/components/section2/Section2.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Section2 = () => {
-  return (
-    <div className='h-screen w-full bg-gray-950'>Section2</div>
-  )
-}
 
-export default Section2
+  const [num, setNum] = useState(0);
+
+  const changeNum = () => {
+
+    setNum((prev) => {
+      console.log(prev + 1);
+      return prev + 1;
+    });
+  };
+
+  return (
+    <div className="h-screen w-full bg-gray-950">
+      <input
+        className="h-10 w-30 bg-white m-10"
+        type="text"
+        onChange={(e) => console.log(e.target.value)}
+        placeholder="Enter Data"
+      />
+
+      <div>
+        <h1>The Value of Num is {num}</h1>
+        <button onClick={changeNum}>click me</button>
+        <div className="div1"></div>
+        <div className="div2"></div>
+        <div className="div3"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Section2;
