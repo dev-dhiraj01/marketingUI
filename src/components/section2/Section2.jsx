0737e82d9d895e2c017f5d47d0dfd0418d0de978
Dhiraj70C <dchaudhari1256@gmail.com>
2026-01-20 21:14:26 +0530

    Created functions for changing the states of variables

--- src/components/section2/Section2.jsx
+++ src/components/section2/Section2.jsx
@@ -1,32 +1,60 @@
 import React, { useState } from "react";
 
 const Section2 = () => {
-
   const [num, setNum] = useState(0);
 
   const changeNum = () => {
-
-    setNum((prev) => {
-      console.log(prev + 1);
-      return prev + 1;
-    });
+    setNum(num + 1);
   };
+const reset = () =>{
+  setNum(0);
+}
 
+var [user,setUser] = useState({name:"Dhiraj", age:22});
+  let flag = true;
+ const showUser = () => {
+
+  if(flag){
+      const newUser = { ...user };
+      newUser.name = "Aman";
+      setUser(newUser);
+      flag = false;
+  }else if(flag == false){
+          const newUser2 = { ...user };
+          newUser2.name = "Dhiraj";
+          setUser(newUser2);
+          flag = true;
+  }
+
+ }
   return (
-    <div className="h-screen w-full bg-gray-950">
-      <input
-        className="h-10 w-30 bg-white m-10"
-        type="text"
-        onChange={(e) => console.log(e.target.value)}
-        placeholder="Enter Data"
-      />
-
-      <div>
-        <h1>The Value of Num is {num}</h1>
-        <button onClick={changeNum}>click me</button>
-        <div className="div1"></div>
-        <div className="div2"></div>
-        <div className="div3"></div>
+    <div className="h-screen w-full bg-gray-950 text-white">
+      <div className="flex justify-center items-center flex-wrap ">
+        <input
+          className="h-10 w-full m-10"
+          type="text"
+          onChange={(e) => console.log(e.target.value)}
+          placeholder="Enter Data"
+        />
+        <h1 className="text-white h-30 w-30 bg-gray-700 rounded-2xl flex justify-center items-center p-5 m-10">
+          The Value of Num is {num}
+        </h1>
+        <button
+          onClick={changeNum}
+          className=" h-10 w-30 m-10 rounded-full bg-cyan-400"
+        >
+          click me
+        </button>
+        <button
+          onClick={reset}
+          className=" h-10 w-30 m-10 rounded-full bg-cyan-400"
+        >
+          Reset
+        </button>
+        <h1>{user.name},{user.age}</h1>
+        <button onClick={showUser} className=" h-10 w-30 m-10 rounded-full bg-cyan-400">
+          User Details
+        </button>
       </div>
     </div>
   );
