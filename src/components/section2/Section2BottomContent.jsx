f8ad9452758a00f19bab28438fcc0c50f5bfd047
Dhiraj70C <dchaudhari1256@gmail.com>
2026-02-01 16:47:05 +0530

    update form

--- src/components/section2/Section2BottomContent.jsx
+++ src/components/section2/Section2BottomContent.jsx
@@ -1,60 +1,45 @@
-import { useState } from "react";
+// import { useState } from "react";
 const Section2BottomContent = () => {
-   const [formData, setFormData] = useState({
-     name: "",
-     email: "",
-     age: "",
-   });
+  //  const [formData, setFormData] = useState({
+  //    name: "",
+  //    email: "",
+  //    age: "",
+  //  });
 
-    const handleChange = (e) => {
-      const { name, value } = e.target;
-      setFormData((prev) => ({
-        ...prev,
-        [name]: value,
-      }));
-    };
+    // const handleChange = (e) => {
+    //   const { name, value } = e.target;
+    //   setFormData((prev) => ({
+    //     ...prev,
+    //     [name]: value,
+    //   }));
+    // };
 
     const handleSubmit = (e) => {
       e.preventDefault();
-      console.log("Submitted Data:", formData);
+      console.log("hello");
+      // console.log("Submitted Data:", formData);
     };
   return (
     <div>
-      <form onSubmit={handleSubmit} className="p-5">
+      <form className="p-5 m-10 bg-gray-600">
         <input
           className="p-5 m-3 text-sm border w-full"
           type="text"
           name="name"
           placeholder="Enter Your Name"
-          value={formData.name}
-          onChange={handleChange}
+          // value={formData.name}
+          onChange={()=>{
+            // handleChange();
+          }}
           required
-        />
-
-        <input
-          className="p-5 m-3 text-sm border w-full"
-          type="email"
-          name="email"
-          placeholder="Enter Your Email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-
-        <input
-          className="p-5 m-3 text-sm border w-full"
-          type="number"
-          name="age"
-          placeholder="Enter Your Age"
-          value={formData.age}
-          onChange={handleChange}
-          min="1"
         />
 
         <button
           type="submit"
-          className="p-5 m-3 bg-cyan-500 text-white rounded"
-          onClick={handleSubmit}
+          className="p-5 m-3 bg-cyan-500 text-white rounded-2xl"
+          onClick={(e) =>{
+            handleSubmit(e);
+          }}
         >
           Submit
         </button>
